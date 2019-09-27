2b2e2e278643422e0fab1162eaab93a5fb12e730
koszuta <koszutan94@gmail.com>
2019-09-27 17:23:16 -0600

    Adding resizing with triangles

--- triangles/triangles.go
+++ triangles/triangles.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
@@ -12,12 +13,15 @@ type Vec2 struct {
 }
 
 var (
-	width, height int
+	resizeCount     int64
+	totalResizeTime time.Duration
+
+	width, height float64
 	window        *pixelgl.Window
 
-	rows, cols int
-	grid       []bool
-	cells      []Vec2
+	rows, cols   int
+	grid         []bool
+	rowTriangles []*pixel.TrianglesData
 )
 
 func main() {
@@ -28,8 +32,9 @@ func run() {
 	monitor_width, monitor_height := pixelgl.PrimaryMonitor().Size()
 	fmt.Printf("%f x %f\n", monitor_width, monitor_height)
 	win, err := pixelgl.NewWindow(pixelgl.WindowConfig{
-		Title:  "Triangles",
-		Bounds: pixel.R(0, 0, 1000, 1000),
+		Title:     "Triangles",
+		Bounds:    pixel.R(0, 0, 1000, 1000),
+		Resizable: true,
 	})
 	if err != nil {
 		panic(err)
@@ -46,17 +51,56 @@ func run() {
 		grid[i] = i%2 == 0
 	}
 
-	width := win.Bounds().Max.X
-	height := win.Bounds().Max.Y
+	rowTriangles = make([]*pixel.TrianglesData, rows)
+	for i := range rowTriangles {
+		rowTriangles[i] = pixel.MakeTrianglesData(2 * 3 * cols)
+	}
+
+	width = win.Bounds().Max.X
+	height = win.Bounds().Max.Y
+	update_vertex_positions()
+
+	for row := 0; row < rows; row++ {
+		triangles := *rowTriangles[row]
+		for col := 0; col < cols; col++ {
+			var color pixel.RGBA
+			if grid[row*cols+col] {
+				color = chartreuse
+			} else {
+				color = black
+			}
+			for i := 0; i < 6; i++ {
+				triangles[6*col+i].Color = color
+			}
+		}
+	}
+
+	for !win.Closed() {
+		if width != win.Bounds().Max.X || height != win.Bounds().Max.Y {
+			width = win.Bounds().Max.X
+			height = win.Bounds().Max.Y
+			update_vertex_positions()
+		}
+		for _, triangles := range rowTriangles {
+			win.MakeTriangles(triangles).Draw()
+		}
+		win.Update()
+	}
+	fmt.Printf("\nAvg resize time %s\n", time.Duration(totalResizeTime.Nanoseconds()/resizeCount))
+}
+
+func update_vertex_positions() {
+	defer func(start time.Time) {
+		resizeCount++
+		elapsed := time.Since(start)
+		totalResizeTime += elapsed
+	}(time.Now())
+	
 	row_height := height / float64(rows)
 	col_width := width / float64(cols)
 	fmt.Printf("row_height=%f col_width=%f\n\n", row_height, col_width)
-
-	rowTriangles := make([]*pixel.TrianglesData, rows)
-	for i := range rowTriangles {
-		rowTriangles[i] = pixel.MakeTrianglesData(2 * 3 * cols)
-		triangles := *rowTriangles[i]
-
+	for i, it := range rowTriangles {
+		triangles := *it
 		triangles[0].Position = pixel.Vec{
 			X: 0,
 			Y: float64(i) * row_height,
@@ -94,29 +138,6 @@ func run() {
 			X: width,
 			Y: float64(i+1) * row_height,
 		}
-	}
-
-	for row := 0; row < rows; row++ {
-		triangles := *rowTriangles[row]
-		for col := 0; col < cols; col++ {
-			var color pixel.RGBA
-			if grid[row*cols+col] {
-				color = chartreuse
-			} else {
-				color = black
-			}
-			for i := 0; i < 6; i++ {
-				triangles[6*col+i].Color = color
-			}
-		}
-	}
-
-	for _, triangles := range rowTriangles {
-		win.MakeTriangles(triangles).Draw()
-	}
-
-	for !win.Closed() {
-		win.Update()
 	}
 }
 
